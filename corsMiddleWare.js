c28393057e8c06904fde056c0f3876de606e5a28
mailtomainak <mailtomainak@gmail.com>
2018-12-24 15:23:13 +0530

    Add header

--- corsMiddleWare.js
+++ corsMiddleWare.js
@@ -4,7 +4,7 @@ const enableCors = (req,res,next)=>{
 
     //temporarily  allowed . TODO change Origin
     res.header("Access-Control-Allow-Origin", config.ALLOWED_ORIGIN);
-    res.header("Access-Control-Allow-Headers", "x-slomo-auth");
+    res.header("Access-Control-Allow-Headers", config.TOKEN_HEADER);
     next();
 }
 
