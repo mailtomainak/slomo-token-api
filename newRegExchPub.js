cfb73a6358d76aff0240c39a7eacfd912435864f
mailtomainak <mailtomainak@gmail.com>
2019-01-09 15:29:03 +0530

    Add persistent flag

--- newRegExchPub.js
+++ newRegExchPub.js
@@ -7,7 +7,7 @@ async function  publishToNewRegistrationExchange(user){
        
         const channel = await connection.createChannel();
         const exchange = await channel.checkExchange(config.AMQP_NEW_REGISTRATION_EXCHANGE);
-        channel.publish(config.AMQP_NEW_REGISTRATION_EXCHANGE,"",Buffer.from(JSON.stringify(user)));
+        channel.publish(config.AMQP_NEW_REGISTRATION_EXCHANGE,"",Buffer.from(JSON.stringify(user)),{persistent:true});
         await channel.close();
     }
     catch (e){
