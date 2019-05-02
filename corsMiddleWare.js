9e21ea8ed27c3b2ea7180e3dee77e9bba9e22556
mailtomainak <mailtomainak@gmail.com>
2019-05-02 13:37:28 +0200

    add secure origin

--- corsMiddleWare.js
+++ corsMiddleWare.js
@@ -3,6 +3,7 @@ const config = require('./config');
 const enableCors = (req,res,next)=>{
 
     //temporarily  allowed . TODO change Origin
+    res.header("Access-Control-Allow-Origin", "https://sincere-name.surge.sh");
     res.header("Access-Control-Allow-Origin", config.ALLOWED_ORIGIN);
     res.header("Access-Control-Allow-Headers", config.TOKEN_HEADER);
     next();
