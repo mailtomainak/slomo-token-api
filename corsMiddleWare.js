50297b70c94c60d82ee17af72a0e436c4b42a876
mailtomainak <mailtomainak@gmail.com>
2019-05-02 13:46:19 +0200

    Rectify Typo

--- corsMiddleWare.js
+++ corsMiddleWare.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 
 const enableCors = (req,res,next)=>{
-
+    
     res.header("Access-Control-Allow-Origin", config.ALLOWED_ORIGIN);
     res.header("Access-Control-Allow-Headers", config.TOKEN_HEADER);
     next();
